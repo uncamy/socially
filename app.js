774840c467dc8d9b43bed52ad4228efc71335c78
uncamy@gmail.com <uncamy@gmail.com>
2015-07-30 19:19:06 -0400

    step 6

--- app.js
+++ app.js
@@ -37,10 +37,21 @@ if (Meteor.isClient) {
 		};
 }]);
 
-  angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams',
-  function($scope, $stateParams){
+  angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
+  function($scope, $stateParams, $meteor){
 
-    $scope.partyId = $stateParams.partyId;
+    $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+    $scope.save = function() {
+  		$scope.party.save().then(function(numberOfDocs){
+        console.log('save success doc affected ', numberOfDocs);
+      }, function(error){
+        console.log('save error', error);
+      });
+    };
+
+	$scope.reset = function() {
+	 	$scope.party.reset();
+	};
 
 }]);
 
