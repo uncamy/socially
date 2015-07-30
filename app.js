b5bc7b05dbf41d4a1537ff3586470fb07c719e3c
uncamy@gmail.com <uncamy@gmail.com>
2015-07-30 18:45:05 -0400

    step 4

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ if (Meteor.isClient) {
   angular.module('socially').controller('PartiesListCtrl', ['$scope', '$meteor',
     function($scope, $meteor){
 
-      $scope.parties = $meteor.collection(Parties);
+    	$scope.parties = $meteor.collection(Parties);
+    	$scope.remove = function(party){
+   			$scope.parties.remove(party);
 
-    }]);
+    	};
+  		$scope.removeAll = function(){
+  			$scope.parties.remove();
+		};
+}]);
 }
 
 if (Meteor.isServer) {
