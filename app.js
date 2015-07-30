f1d17d8023404bc64412493f90d0b6da1690dbac
uncamy@gmail.com <uncamy@gmail.com>
2015-07-30 18:20:05 -0400

    added mongodb

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient) {
-	angular.module('socially',['angular-meteor']);
 
-    angular.module('socially').controller('PartiesListCtrl', ['$scope',
-        function($scope){
+  angular.module('socially',['angular-meteor']);
 
-          $scope.parties = [
-            {'name': 'Dubstep-Free Zone',
-              'description': 'Can we please just for an evening not listen to dubstep.'},
-            {'name': 'All dubstep all the time',
-              'description': 'Get it on!'},
-            {'name': 'Savage lounging',
-              'description': 'Leisure suit required. And only fiercest manners.'}
-          ];
+  angular.module('socially').controller('PartiesListCtrl', ['$scope', '$meteor',
+    function($scope, $meteor){
 
-      }]);
-  }
+      $scope.parties = $meteor.collection(Parties);
+
+    }]);
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Parties.find().count() === 0) {
+
+      var parties = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Can we please just for an evening not listen to dubstep.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+
+      for (var i = 0; i < parties.length; i++)
+        Parties.insert(parties[i]);
+
+    }
+   });
+}
