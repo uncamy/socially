3796e7079d03c84335cf803aa74b4b507b365ab8
uncamy@gmail.com <uncamy@gmail.com>
2015-08-04 11:33:03 -0400

    added images

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,35 +1,27 @@
 angular.module('socially',[
-      'angular-meteor',
-      'ui.router',
-      'angularUtils.directives.dirPagination',
-      'uiGmapgoogle-maps', 
-      'ngMaterial'
-    ]);
-var themeIcons = function ($mdIconProvider) {
+  'angular-meteor',
+  'ui.router',
+  'angularUtils.directives.dirPagination',
+  'uiGmapgoogle-maps',
+  'ngMaterial',
+  'ngFileUpload',
+  'ngImgCrop',
+  'xeditable',
+  'angular-sortable-view'
+]);
+
+var themeIcons = ['$mdIconProvider' , function ($mdIconProvider) {
 
   $mdIconProvider
-    .iconSet("social",
-             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
+    .iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
+    .iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
+    .iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
+    .iconSet("content", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
+    .iconSet("toggle", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
+    .iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
+    .iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
 
-    .iconSet("action",
-             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
-
-    .iconSet("communication",
-             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
-
-    .iconSet("content",
-             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
-
-    .iconSet("toggle",
-             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
-
-    .iconSet("navigation",
-             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
-
-    .iconSet("image",
-             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
-
-};
+}];
 
 angular.module('socially')
   .config(themeIcons);
